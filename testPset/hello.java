666958e944c617457148694fdd54176c16d94451
koshaughnesseyRye <katie_oshaughnessey@ryecountryday.org>
2019-05-20 07:36:10 -0400

    Removed solution code.  Added comments

--- testPset/hello.java
+++ testPset/hello.java
@@ -3,12 +3,9 @@ public class Greetings2
 {
   public static void main(String[ ] args)
   {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Enter your first name: ");
-    String firstName = in.nextLine( );
-    System.out.print("Enter your last name: ");
-    String lastName = in.nextLine( );
-    System.out.println("Hello, " + firstName + " " + lastName);
-    in.close();
+    // TODO: Enter your code to print your name here 
+    // do not forget to check syntax, such as parentheses, quotation marks and semi colons!
+
+    
   }
 }
